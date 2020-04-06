12e120dfa29975a0954dbde9979ec1db6e59a8b7
FAHEEM133 <54945240+FAHEEM133@users.noreply.github.com>
2020-04-06 20:59:51 +0530

    Add files via upload

---
+++ faheem-question2.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<math>
+using namespace std;
+int main()
+{
+int num,d=0,n,r,p=0,a=1;
+char choice;
+cin>>num;
+cin>>choice;
+switch(choice)
+{
+case a:
+n=num;
+while(n==0)
+{
+n=n/10;
+d++;
+}
+n=num;
+while(n!=0)
+{
+r=n%10;
+a=a*pow(r,d);
+n=n/10;
+}
+cout<<a<<"\n"; 
+break; 
+case p:
+n=num;
+while(n!=0)
+{
+r=n%10;
+p=p+10+r;
+n=n/10;
+}
+cout<<p<<"\n";
+break;
+}
+}
